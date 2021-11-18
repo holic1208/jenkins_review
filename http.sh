2a673bb69857a2584ee118f15c51f65d3bec2159
suuu <holic1208@daum.net>
2021-11-18 21:15:19 +0900

    TT

--- http.sh
+++ http.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yum install -y httpd
 chmod 777 /var/www/html
-echo "WECOME 80 PORT~!~!!" >> /var/www/html/index.html
+echo "WECOME 80 PORT~###!~!!" >> /var/www/html/index.html
 systemctl start httpd
